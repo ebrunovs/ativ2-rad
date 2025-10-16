1909e20ef7bf073040df8df97e34184383e5f545
Bruno Eneas <brunovn.eneas@gmail.com>
2025-10-15 22:34:40 -0300

    feat: add models and admin configurations

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Autor, Editora, Livro, Publica
 
-# Register your models here.
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+	list_display = ('id', 'nome')
+	search_fields = ('nome',)
+
+@admin.register(Editora)
+class EditoraAdmin(admin.ModelAdmin):
+	list_display = ('id', 'nome')
+	search_fields = ('nome',)
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+	list_display = ('id', 'ISBN', 'titulo', 'publicacao', 'preco', 'estoque', 'editora')
+	search_fields = ('ISBN', 'titulo')
+
+@admin.register(Publica)
+class PublicaAdmin(admin.ModelAdmin):
+	list_display = ('id', 'livro', 'autor')
+	search_fields = ('livro__titulo', 'autor__nome')
